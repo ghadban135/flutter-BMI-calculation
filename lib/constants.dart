abfe60254ae6da0592b29381599037dac0306d95
ahmad <ah.ghadban8@gmail.com>
2020-06-08 23:53:06 +0300

    create first section of input page plus add some style

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+const kActiveCardColour = Color(0xFFFFD700);
+
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  fontWeight: FontWeight.w500,
+  color: Color(0xFF4169E1),
+);
+
+const kNumberTextStyle = TextStyle(
+    fontSize: 40.0,
+    fontWeight: FontWeight.w500,
+    color: Colors.black
+);
--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -9,6 +11,63 @@ class _InputPageState extends State<InputPage> {
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+      Expanded(
+      child: ReusableCard(
+        colour: kActiveCardColour,
+        cardChild: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'HEIGHT',
+              style: kLabelTextStyle,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: <Widget>[
+                Text(
+                  height.toString(),
+                  style: kNumberTextStyle,
+                ),
+                Text(
+                  'cm',
+                  style: kLabelTextStyle,
+                )
+              ],
+            ),
+            SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                inactiveTrackColor: Color(0xFF8D8E98),
+                activeTrackColor: Colors.white,
+                thumbColor:  Color(0xFF4169E1),
+                overlayColor: Color(0x29EB1555),
+                thumbShape:
+                RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                overlayShape:
+                RoundSliderOverlayShape(overlayRadius: 30.0),
+              ),
+              child: Slider(
+                value: height.toDouble(),
+                min: 120.0,
+                max: 220.0,
+                onChanged: (double newValue) {
+                  setState(() {
+                    height = newValue.round();
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
   }
 }
